c9eac00a268da04e42f7fe8db46df801af40542e
Awais Javaid <53362912+awaisjavaid930@users.noreply.github.com>
2023-10-06 03:28:37 +0500

    login form done

--- quasar-ikonic/src/pages/IndexPage.vue
+++ quasar-ikonic/src/pages/IndexPage.vue
@@ -1,17 +1,36 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
-  </q-page>
+  <q-page-container>
+    <q-page class="flex-center">
+      <div class="q-pa-lg">
+        <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md q-px-lg">
+          <q-input filled v-model="formData.email" label="Your Email *" lazy-rules
+            :rules="[val => val && val.length > 0 || 'Please type email']" />
+
+          <q-input filled v-model="formData.password" type="text" label="Your Password *" lazy-rules :rules="[
+            val => val !== null && val !== '' || 'Please type your password',
+            val => val > 0 && val < 100 || 'Please type a real age'
+          ]" />
+          <div>
+            <q-btn label="Submit" type="submit" color="primary" />
+            <q-btn label="Register" @click="$router.push('/register')" color="primary" flat class="q-ml-sm" />
+          </div>
+        </q-form>
+
+      </div>
+    </q-page>
+  </q-page-container>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
-
-export default defineComponent({
-  name: 'IndexPage'
-})
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      formData: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+}
 </script>
---
+++ quasar-ikonic/src/pages/RegisterPage.vue
@@ -0,0 +1,37 @@
+<template>
+    <q-page-container>
+        <q-page class="flex-center">
+            <div class="q-pa-lg">
+                Register Page
+                <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md q-px-lg">
+                    <q-input filled v-model="formData.email" label="Your Email *" lazy-rules
+                        :rules="[val => val && val.length > 0 || 'Please type email']" />
+
+                    <q-input filled v-model="formData.password" type="text" label="Your Password *" lazy-rules :rules="[
+                        val => val !== null && val !== '' || 'Please type your password',
+                        val => val > 0 && val < 100 || 'Please type a real age'
+                    ]" />
+                    <div>
+                        <q-btn label="Submit" type="submit" color="primary" />
+                        <q-btn label="Register" to="/register" color="primary" flat class="q-ml-sm" />
+                    </div>
+                </q-form>
+
+            </div>
+        </q-page>
+    </q-page-container>
+</template>
+
+<script>
+export default {
+    name: 'RegisterPage',
+    data() {
+        return {
+            formData: {
+                email: '',
+                password: ''
+            }
+        }
+    },
+}
+</script>
